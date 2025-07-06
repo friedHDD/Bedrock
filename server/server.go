962efe29323341156bd90331bb754dfde66a77a2
friedHDD <8622634@gmail.com>
2025-07-06 08:10:19 +0800

    feat: Enable public directory

--- server/server.go
+++ server/server.go
@@ -2,36 +2,44 @@ package server
 
 import (
 	"github.com/friedHDD/Bedrock/handler"
+	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"path/filepath"
 )
 
 func Start() {
 
 	r := gin.Default()
+	r.StaticFS("/assets", http.Dir("./dist/assets"))
 
-	r.GET("/api/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+	api := r.Group("/api")
+	{
+		api.GET("/ping", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
 		})
-	})
 
-	r.GET("/api/list", func(c *gin.Context) {
-		handler.ListDirectoryHandler(c)
-	})
+		api.GET("/list", func(c *gin.Context) {
+			handler.ListDirectoryHandler(c)
+		})
 
-	r.GET("/api/download", func(c *gin.Context) {
-		handler.DownloadHandler(c)
-	})
+		api.GET("/download", func(c *gin.Context) {
+			handler.DownloadHandler(c)
+		})
 
-	r.GET("/api/file/details", func(c *gin.Context) {
-		handler.FileDetailHandler(c)
-	})
+		api.GET("/file/details", func(c *gin.Context) {
+			handler.FileDetailHandler(c)
+		})
 
-	r.GET("/api/ipfs/add", func(c *gin.Context) {
-		handler.IPFSAddHandler(c)
+		api.GET("/ipfs/add", func(c *gin.Context) {
+			handler.IPFSAddHandler(c)
+		})
+	}
+	r.NoRoute(func(c *gin.Context) {
+		indexPath := filepath.Join("./dist", "index.html")
+		c.File(indexPath)
 	})
 	log.Printf("Bedrock starting, listening on :9090")
 
